25f2abc555fe1d6d7f912f0087867ddf61f30da7
John <likisongproduct@gmail.com>
2020-05-10 00:29:46 -0700

    removing unnecessary fields

--- getStats.py
+++ getStats.py
@@ -4,17 +4,17 @@ import json
 # stephlovesbecca summoner ID
 myId = "Ry-EABKsOzN8GMcayrr_8zA7t1JzaSm51SZ6LZxj_ykj0w"
 # number of matches to look back on 
-endIndex = "10"
+endIndex = "1"
 # LoL game version
 version = "10.9.1"
-api_key = "RGAPI-418a74e7-b129-4ae4-b4b8-9ff4a1b0e6f3"
+api_key = "RGAPI-2c55a0ac-16be-4710-b0a8-344daa8b9c77"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.9",
     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
     "Origin": "https://developer.riotgames.com",
-    "X-Riot-Token": "RGAPI-418a74e7-b129-4ae4-b4b8-9ff4a1b0e6f3"
+    "X-Riot-Token": api_key
 }
 
 # requesting json of champion data on LoL
@@ -25,7 +25,7 @@ champions = champions["data"]
 
 matchlist_url = "https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/" + myId + "?endIndex=" + endIndex + "&api_key=" + api_key
 response = requests.get(matchlist_url, headers=headers)
-# print(response.json())
+print(response.json())
 response = response.json()["matches"]
 retVal = []
 
@@ -35,6 +35,19 @@ for match in response:
     match_url = "https://na1.api.riotgames.com/lol/match/v4/matches/" + matchId
     matchResponse = requests.get(match_url, headers=headers)
     matchResponse = matchResponse.json()
+
+    matchResponse.pop("queueId")
+    matchResponse.pop("gameCreation")
+    matchResponse.pop("gameDuration")
+    matchResponse.pop("mapId")
+    matchResponse.pop("platformId")
+    matchResponse.pop("gameType")
+    
+    for player in matchResponse["participants"]:
+        player.pop("stats")
+        player.pop("timeline")
+        player.pop("spell1Id")
+        player.pop("spell2Id")
     
     for team in matchResponse["teams"]:
         # print(team["bans"])
